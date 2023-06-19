7ae7c0c921de24ed300108cc91992c0d247452df
SurajSingh45 <suraj.12012199@lpu.in>
2023-06-19 11:54:33 +0530

    assignment 1 solutions

---
+++ ASSIGNMENT/Assignment1/Quesiton5.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const router = express.Router();
+const Todo = require('../models/todo');
+
+router.delete('/api/todo/:id', async (req, res) => {
+  try {
+    const { id } = req.params;
+    const todo = await Todo.findById(id);
+    if (!todo) {
+      return res.status(404).json({ message: 'Todo not found' });
+    }
+    await todo.remove();
+    res.json({ message: 'Todo deleted' });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
+
+module.exports = router;
